ec6c5965b9264fc7e5f290d8ba12491572ab41d1
JavaRubyNode <viniciusjavaruby@gamail.com>
2016-09-16 13:46:15 -0300

    Meu Web services

---
+++ src/main/java/br/com/vinicius/LifeBloodApiRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.vinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeBloodApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeBloodApiRestApplication.class, args);
+	}
+}
